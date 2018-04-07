58599db4105922ae9d27ec7d4ddba70ad98619c2
Tyler Bratton <tylerbratton96@gmail.com>
2018-04-06 20:32:50 -0400

    type fix

--- start_deviantart_scraper.sh
+++ start_deviantart_scraper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # commandline arguments are (in this order):
-# grallery_url directory
+# gallery_url directory
 # where gallery_url is the url of the gallery to download, and directory is
 # the directory in your home folder to save the files to.
 # example:
